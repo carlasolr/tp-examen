1d739c6ea6ae4a8c29a88fb715a16aa37f92dc7d
carlasolr <64711799+carlasolr@users.noreply.github.com>
2020-05-26 15:29:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /*
 Esta funcion recibe la lista de objetos libros y un numero que representa al
 codigo ISBN que identifica a los ejemplares. Se define una variable objeto, 
-luego se recorre la lista, identifica al elemento libro, constrasta el parametro numerico con el ISBN del libro en cuestion y si estos coinciden, se le cargan
-propiedades del libro a la variable del principio y se muestra. Una de estas
-propiedades esta definida con el llamado a una funcion del utils.js
+luego se recorre la lista, identifica al elemento libro, constrasta el parametro numerico con el ISBN del libro 
+en cuestion y si estos coinciden, se le cargan propiedades del libro a la variable del principio
+y se muestra. Una de estas propiedades esta definida con el llamado a una funcion del utils.js
 */
 
 function averiguarDisponibilidad(lista, numero){
@@ -29,7 +29,10 @@ function averiguarDisponibilidad(lista, numero){
 ///////////////////
 
 /*
-la funcion recibe la lista de libros y el nombre de un autor. asi como las demas, posee un array vacio definido al principio, recorre la lista, evalua el cumplimiento de dos condiciones (que el parametro autor coincida con el autor del libro y que el libro en cuestion este disponible) y si ambas efectivamente suceden, carga al array el/los libro/s que califica/n para ambas condiciones.
+la funcion recibe la lista de libros y el nombre de un autor. asi como las demas, posee un array vacio definido al principio,
+recorre la lista, evalua el cumplimiento de dos condiciones (que el parametro autor coincida con el autor del libro y
+que el libro en cuestion este disponible) y si ambas efectivamente suceden, carga al array el/los libro/s que califica/n 
+para ambas condiciones.
 */
 
 function librosDeAutor(lista, autor){
@@ -49,7 +52,12 @@ function librosDeAutor(lista, autor){
 /////////////////
 
 /*
-funcion que recibe la lista de libros, tiene un array vacio definido al principio, recorre la lista, define un acumulador que se llenara con todos los pedidos que cada libro tuvo, se abre otra iteracion que esta vez recorre a la propiedad array pedidos que posee cada libro para recopilar justamente la cantidad de pedidos, estos se suman al acumulador. Al array vacio del principio se le cargara un objeto con las propiedades titulo y el numero de veces que fue alquilado, armandose asi una lista de objetos con cada libro. Dado que es un ranking de los 10 mas pedidos, se ordenara esa lista usando el metodo sort y luego se seleccionara a los primeros diez elementos 
+funcion que recibe la lista de libros, tiene un array vacio definido al principio, recorre la lista, define un acumulador
+que se llenara con todos los pedidos que cada libro tuvo, se abre otra iteracion que esta vez recorre a la propiedad array
+pedidos que posee cada libro para recopilar justamente la cantidad de pedidos, estos se suman al acumulador. 
+Al array vacio del principio se le cargara un objeto con las propiedades titulo y el numero de veces que fue alquilado, 
+armandose asi una lista de objetos con cada libro. Dado que es un ranking de los 10 mas pedidos, se ordenara esa lista 
+usando el metodo sort y luego se seleccionara a los primeros diez elementos 
 */
 
 function losMasPedidos(lista) {
